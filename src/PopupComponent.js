b00697c0e682b1f24820924f1562bc9aec7ce700
Brycen Medart <brycemedart@gmail.com>
2022-02-11 22:22:51 -0800

    Change name of component *add functionality for close btn toggle

---
+++ src/PopupComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PopupComponent = (props) => {
+
+  let [btnClick, setBtnClick] = React.useState(false);
+
+  const MenuToggle = () => {
+    (btnClick === false ? setBtnClick(true) : setBtnClick(false))
+  }
+
+  return (
+    <section className={btnClick === true ? 'show' : 'hide'}>
+      <div className="popup-overlay">
+        <h2 className={props.type}> {props.title} </h2>
+        <p>{props.body}</p>
+        <button className={props.type} onClick={MenuToggle}> Close</button>
+      </div>
+    </section>
+  )
+}
+export default PopupComponent;
